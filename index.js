362caf7ba71a6fec66a2493bbb1e3be3ba5324b9
Yurii Obrevko <smile.oym@gmail.com>
2018-07-14 11:41:52 +0300

    first React-component Hello World

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { getName } from './src/toCount';
+import Hello from './src/Hello';
 import add from './src/toCount';
 
 console.log(getName());
 console.log(add(23,56));
 
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root')
+);
